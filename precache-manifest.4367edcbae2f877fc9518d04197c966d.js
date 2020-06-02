52eba0fd02e1dbc5311250d8a999d9f9fef82c2b
resistance90 <61583947+resistance90@users.noreply.github.com>
2020-06-02 22:06:47 +0300

    first upload
    first upload

---
+++ precache-manifest.4367edcbae2f877fc9518d04197c966d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2540b0465399f8a299371ee2f98461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c0b28ace96a0ad964d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0bef32af27b1c8a3209f",
+    "url": "/static/css/main.8e264a19.chunk.css"
+  },
+  {
+    "revision": "91c0b28ace96a0ad964d",
+    "url": "/static/js/2.db07dd38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.db07dd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bef32af27b1c8a3209f",
+    "url": "/static/js/main.dfcb0c13.chunk.js"
+  },
+  {
+    "revision": "7537dfba6d2c16d92562",
+    "url": "/static/js/runtime-main.08046236.js"
+  }
+]);
